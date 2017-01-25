05759b65c2c5b7f49877f966a941d9a586caeae8
wsn <wsn@bsfit.com.cn>
2017-01-25 17:50:50 +0800

    隐藏欢迎界面中的的电池等状态栏

--- app/src/main/java/com/coolweather/app/coolweather/actiivity/StartActivity.java
+++ app/src/main/java/com/coolweather/app/coolweather/actiivity/StartActivity.java
@@ -1,13 +1,12 @@
 package com.coolweather.app.coolweather.actiivity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.coolweather.app.coolweather.R;
 import com.coolweather.app.coolweather.application.MarkApplication;
@@ -18,9 +17,10 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         //judge if we have to skip the welcome view.
-        if(MarkApplication.SKIP_WELCOME) {
+        if (MarkApplication.SKIP_WELCOME) {
             Intent intent = new Intent(StartActivity.this, BasicShowActivity.class);
             startActivity(intent);
             finish();
@@ -29,8 +29,8 @@ public class StartActivity extends AppCompatActivity {
         MarkApplication.SKIP_WELCOME = true;
 
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 0f);
-        alphaAnimation.setDuration(5000);
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 1.0f);
+        alphaAnimation.setDuration(2000);
         alphaAnimation.setBackgroundColor(0);
         alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
             @Override
@@ -50,14 +50,7 @@ public class StartActivity extends AppCompatActivity {
 
             }
         });
-        //to force the image view stay the last 
         alphaAnimation.setFillAfter(true);
         imageView.setAnimation(alphaAnimation);
     }
-    // cancel all click events when it is entering the welcome view
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        return false;
-//    }
-
 }
